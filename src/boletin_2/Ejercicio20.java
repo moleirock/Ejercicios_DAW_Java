1d5edd61367de2bf1d0b512c8300188c3e6ab588
Moleiro <alberto.moleiro7@gmail.com>
2022-02-14 11:37:48 +0100

    Enunciados y ejercicio 20 completado

--- src/boletin_2/Ejercicio20.java
+++ src/boletin_2/Ejercicio20.java
@@ -1,10 +1,27 @@
 package boletin_2;
-
+import java.util.*;
 public class Ejercicio20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		20. Pedir un número N, introducir N sueldos, y mostrar el sueldo máximo.
+		Scanner sc=new Scanner(System.in);
+		double sueldo,max;
+		System.out.println("Introduce los sueldos y te diré el máximo, cuando quieras terminar escribe un 0 ");
+		sueldo=sc.nextDouble();
+		max=sueldo;
+		while(sueldo!=0) {
+		while(sueldo<0) {
+			System.out.println("El sueldo debe ser positivo");
+			sueldo=sc.nextDouble();
+		}
+		if(sueldo>max) {
+			max=sueldo;
+		}
+		sueldo=sc.nextDouble();
+		}
+		System.out.println("El sueldo máximo es " + max);
+		
+		sc.close();
 	}
 
 }
